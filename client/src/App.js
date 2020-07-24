4d1f4a7e9db445d3fc48bef6e0be6e88539e8f04
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-24 08:39:54 -0700

    set up axiosWithAuth and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import './styles.scss';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+      <div className='App'>
+        <Route exact path='/' component={Login} />
+        {/*
+          Build a PrivateRoute component that will
+          display BubblePage when you're authenticated
         */}
       </div>
     </Router>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+// make a post request to retrieve a token from the api
+// when you have handled the token, navigate to the BubblePage route
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const initialState = {
+    username: '',
+    password: '',
+  };
+
+  const [loginCredentials, setLoginCredentials] = useState(initialState);
+
+  const handleChange = () => {};
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+
+      <form>
+        <input
+          type='text'
+          name='username'
+          placeholder='Username'
+          value=''
+          onChange=''
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password'
+          value=''
+          onChange=''
+        />
+      </form>
     </>
   );
 };
